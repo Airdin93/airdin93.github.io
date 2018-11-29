3f1755a36ff7ee5148de89ad94782c85ffe7074d
Airdin93 <emil.johansen93@gmail.com>
2018-11-29 14:44:55 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
+function hamburgerMenuFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
         x.style.display = "none";
     } else {
         x.style.display = "block";
     }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function backToTopFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
