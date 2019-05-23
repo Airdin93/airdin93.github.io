ce4fd4c4c5487b8525f445a463e9dfaac1c2afe1
Airdin93 <emil.johansen93@gmail.com>
2019-05-23 16:24:44 +0200

    test

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 var animation = bodymovin.loadAnimation({
     container: document.getElementById('bm'),
     renderer: 'svg',
-    loop: true,
-    autoplay: true,
+    loop: false,
+    autoplay: false,
     path: 'data.json'   
 })
+
+bm.addEventListener("mouseenter", function () {
+  animation.play();
+  animation.loop = true;
+});
+    
+    /*
+    bm.addEventListener("mouseleave", function () {
+    animation.stop();
+    });
+    */
+
+bm.addEventListener("mouseleave", function () {
+  animation.loop = false;
+});
